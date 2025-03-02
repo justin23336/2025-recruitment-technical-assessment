72b6613770e243edf00508ffc4364e4d31617b29
Justin Prasad <z5534126@unsw.ad.edu.au>
2025-03-02 22:12:23 +1100

    completed task 1 and task 2

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook = new Map<string, any>();
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -45,16 +45,56 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  recipeName = recipeName.replace(/[-_]+/g, " ");
+    
+  recipeName = recipeName.replace(/[^a-zA-Z ]/g, "");
+  
+  recipeName = recipeName.trim().replace(/\s+/g, " ");
+  
+  recipeName = recipeName.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+  
+  return recipeName.length > 0 ? recipeName : null;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const entry = req.body;
 
+  // Validate request body exists
+  if (!entry) {
+    return res.status(400).send("Request body is missing.");
+  }
+
+  // Validate type
+  if (entry.type !== "recipe" && entry.type !== "ingredient") {
+    return res.status(400).send("type can only be \"recipe\" or \"ingredient\".");
+  }
+
+  // Validate ingredient cookTime
+  if (entry.type === "ingredient" && entry.cookTime < 0) {
+    return res.status(400).send("cookTime can only be greater than or equal to 0");
+}
+
+  // Validate uniqueness
+  if (!entry.name || cookbook.has(entry.name)) {
+    return res.status(400).send("entry names must be unique");
+  }
+
+  // Validate requiredItems for recipes
+  if (entry.type === "recipe") {
+    const seenItems = new Set();
+    for (const item of entry.requiredItems) {
+      if (seenItems.has(item.name)) {
+          return res.status(400).send("Recipe requiredItems can only have one element per name.");
+      }
+      seenItems.add(item.name);
+    }
+  }
+  
+
+  cookbook.set(entry.name, entry);
+  return res.status(200).send();
 });
 
 // [TASK 3] ====================================================================
